a9b35f2fbf333dfde7e1101d173caeebde0bd015
VICTOR HUGO DOS SANTOS <victor.santos267@senaisp.edu.br>
2026-08-14 19:55:57 -0300

    Projeto Maven

---
+++ Back/ecommerce-pedidos-break/src/main/java/com/example/ecommercepedidosbreak/EcommercePedidosBreakApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercepedidosbreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePedidosBreakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePedidosBreakApplication.class, args);
+    }
+
+}
